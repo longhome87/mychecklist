8f8b827ae399ac800152af82df647fbf2b4139f3
Nguyen Hoang Long <nhlong2@tma.com.vn>
2021-01-09 11:03:06 +0700

    Upload image to cloudinary for member

--- src/app/members/form-update/form-update.component.ts
+++ src/app/members/form-update/form-update.component.ts
@@ -6,6 +6,8 @@ import { AlertService } from '../../_services';
 import { AuthenticationService } from 'src/app/_services';
 import { ChecklistService } from 'src/app/_firebases/checklist.service';
 import { IMemberAbsent } from 'src/app/_models';
+import { FileUploader, FileUploaderOptions } from 'ng2-file-upload';
+import { Cloudinary } from '@cloudinary/angular-5.x';
 
 @Component({
   selector: 'app-form-update',
@@ -35,6 +37,8 @@ export class FormUpdateComponent implements OnInit {
   province = null;
   private IdCheckList: string;
   private listMember: Array<IMemberAbsent>;
+  private uploader: FileUploader;
+  private response: any;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -43,7 +47,8 @@ export class FormUpdateComponent implements OnInit {
     private router: Router,
     private alertService: AlertService,
     public authenticationService: AuthenticationService,
-    private checklistService: ChecklistService) {
+    private checklistService: ChecklistService,
+    private cloudinary: Cloudinary) {
   }
 
   ngOnInit() {
@@ -56,7 +61,7 @@ export class FormUpdateComponent implements OnInit {
         checklistItem.id = docChangeAction.payload.doc.id;
         if (checklistItem.class && checklistItem.class.id === idCatechism) {
           this.IdCheckList = checklistItem.id;
-          if ( checklistItem.members ) {
+          if (checklistItem.members) {
             this.listMember = checklistItem.members;
           }
         }
@@ -83,51 +88,53 @@ export class FormUpdateComponent implements OnInit {
     const id = this.route.snapshot.params['id'];
     if (id) {
       this.memberService.getMember(id)
-      .subscribe(doc => {
-        let member:any = doc.payload.data();
-        this.saintName = member.saintName;
-        this.firstName = member.firstName;
-        this.lastName = member.lastName;
-        this.id =  id;
-        if (member.nickname) {
-          this.nickname =  member.nickname;
-        }
-        if (member.avatar) {
-          this.avatar =  member.avatar;
-        }
-        this.avatar = member.avatar;
-        if (member.phoneNumber) {
-          this.phoneNumber =  member.phoneNumber;
-        }
-        if (member.dateOfBirth) {
-          this.dateOfBirth =  member.dateOfBirth;
-        }
-        if (member.address) {
-          this.address =  member.address;
-        }
-        if (member.fullNameDad) {
-          this.fullNameDad =  member.fullNameDad;
-        }
-        if (member.phoneNumberDad) {
-          this.phoneNumberDad =  member.phoneNumberDad;
-        }
-        if (member.fullNameMom) {
-          this.fullNameMom =  member.fullNameMom;
-        }
-        if (member.phoneNumberMom) {
-          this.phoneNumberMom =  member.phoneNumberMom;
-        }
-        if (member.parish) {
-          this.parish =  member.parish;
-        }
-        if (member.province) {
-          this.province =  member.province;
-        }
-      })
+        .subscribe(doc => {
+          let member: any = doc.payload.data();
+          this.saintName = member.saintName;
+          this.firstName = member.firstName;
+          this.lastName = member.lastName;
+          this.id = id;
+          if (member.nickname) {
+            this.nickname = member.nickname;
+          }
+          if (member.avatar) {
+            this.avatar = member.avatar;
+          }
+          this.avatar = member.avatar;
+          if (member.phoneNumber) {
+            this.phoneNumber = member.phoneNumber;
+          }
+          if (member.dateOfBirth) {
+            this.dateOfBirth = member.dateOfBirth;
+          }
+          if (member.address) {
+            this.address = member.address;
+          }
+          if (member.fullNameDad) {
+            this.fullNameDad = member.fullNameDad;
+          }
+          if (member.phoneNumberDad) {
+            this.phoneNumberDad = member.phoneNumberDad;
+          }
+          if (member.fullNameMom) {
+            this.fullNameMom = member.fullNameMom;
+          }
+          if (member.phoneNumberMom) {
+            this.phoneNumberMom = member.phoneNumberMom;
+          }
+          if (member.parish) {
+            this.parish = member.parish;
+          }
+          if (member.province) {
+            this.province = member.province;
+          }
+        })
     } else {
       console.log("create member");
       this.isNew = true;
     }
+
+    this.configUploaderToCloudinary();
   }
 
   public hasError = (controlName: string, errorName: string) => {
@@ -180,15 +187,15 @@ export class FormUpdateComponent implements OnInit {
             members: [
               ...listMember,
               {
-              id :data.id,
-              absentDates: null
+                id: data.id,
+                absentDates: null
               }
             ]
           }
           this.checklistService.updateChecklistItem(parmasCheckList)
-          .then(data => {
-            this.router.navigate(['/members']);
-          })
+            .then(data => {
+              this.router.navigate(['/members']);
+            })
         })
         .catch(error => {
           console.log(error);
@@ -196,13 +203,13 @@ export class FormUpdateComponent implements OnInit {
     } else {
       params.id = id;
       this.memberService.updateMember(params)
-      .then(data => {
-        console.log(data, "update");
-        this.router.navigate(['/members']);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+        .then(data => {
+          console.log(data, "update");
+          this.router.navigate(['/members']);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
 
@@ -210,15 +217,19 @@ export class FormUpdateComponent implements OnInit {
     this.router.navigate(['/members']);
   }
 
-  handlerAvatar($event) {
+  handlerAvatar(event) {
     let self = this;
-    let file = $event.target.files[0];
-    let fileReader = new FileReader();
-    if (file.size > 1000000) {
-      this.alertService.error('Chọn lại ảnh, chọn ảnh dưới 1MB');
+    let file = event.target.files[0];
+    if (!file) {
       return;
     }
-    fileReader.onloadend = function(e){
+    
+    let fileReader = new FileReader();
+    // if (file.size > 1000000) {
+    //   this.alertService.error('Chọn lại ảnh, chọn ảnh dưới 1MB');
+    //   return;
+    // }
+    fileReader.onloadend = function (e) {
       self.avatar = fileReader.result.toString();
       console.log(self.avatar, "avatar");
 
@@ -232,5 +243,57 @@ export class FormUpdateComponent implements OnInit {
     this.showShortDesciption = !this.showShortDesciption;
     var element = document.getElementsByClassName("content-form");
     element[0].classList.toggle("toggle-show-form");
+  }
+
+  onSave() {
+    console.log('saved');
+
+    // Remove all the item on queue except the last item
+    this.uploader.queue.splice(0, this.uploader.queue.length - 1);
+    this.uploader.uploadAll();
+  }
+
+  configUploaderToCloudinary() {
+    // Create the file uploader, wire it to upload to your account
+    const uploaderOptions: FileUploaderOptions = {
+      url: `https://api.cloudinary.com/v1_1/${this.cloudinary.config().cloud_name}/upload`,
+      // Upload files automatically upon addition to upload queue
+      autoUpload: false,
+      // Use xhrTransport in favor of iframeTransport
+      isHTML5: true,
+      // Calculate progress independently for each uploaded file
+      removeAfterUpload: true,
+      // XHR request headers
+      headers: [
+        {
+          name: 'X-Requested-With',
+          value: 'XMLHttpRequest'
+        }
+      ]
+    };
+
+    this.uploader = new FileUploader(uploaderOptions);
+
+    this.uploader.onBuildItemForm = (fileItem: any, form: FormData): any => {
+      // Add Cloudinary's unsigned upload preset to the upload form
+      form.append('upload_preset', 'checklist-member');
+      // Add built-in and custom tags for displaying the uploaded photo in the list
+      let tags = 'checklist-member';
+      // Add custom tags
+      form.append('tags', tags);
+      // Add file to upload
+      form.append('file', fileItem);
+
+      // Use default "withCredentials" value for CORS requests
+      fileItem.withCredentials = false;
+      return { fileItem, form };
+    };
+
+    this.uploader.response.subscribe((res: any) => {
+      this.response = JSON.parse(res);
+      console.log(this.response);
+      this.avatar = this.response.url;
+      this.onSubmit();
+    });
   }
 }
